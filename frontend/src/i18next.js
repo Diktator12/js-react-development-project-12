2bdd932ca56d393890e02a56abe2361ba3fa8a3f
Diktator12 <tkachev.twilight@yandex.ru>
2025-09-30 11:41:38 +0300

    add i18next init

---
+++ frontend/src/i18next.js
@@ -0,0 +1,16 @@
+import i18next from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import resources from './locales/index'
+
+i18next
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    pluralSeparator: '_',
+  })
+
+export default i18next
