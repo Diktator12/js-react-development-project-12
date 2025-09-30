bd5eae02126be60edbb950ae9072224a6abad7e0
Diktator12 <tkachev.twilight@yandex.ru>
2025-09-30 11:25:01 +0300

    add provider and imports

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,10 +1,18 @@
 /* eslint-disable functional/no-expression-statement */
 import ReactDOM from 'react-dom/client';
-import init from './init.jsx';
+import init from './i18next.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import store from './app/store.js';
+import App from './App.jsx';
 
 const app = async () => {
   const root = ReactDOM.createRoot(document.querySelector('#chat'));
-  root.render(await init());
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 };
 
 app();
